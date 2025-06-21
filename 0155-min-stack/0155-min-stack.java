e7939c0d0844034df32c4608a50318ba808d282f
Sudharsan R <sudharsanr.22cse@kongu.edu>
2025-06-21 15:28:39 +0530

    Time: 4 ms (97.37%), Space: 45.2 MB (26.04%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,30 @@
+class MinStack {
+
+    // Stack of int arrays: [value, minSoFar]
+    Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int currentMin = stack.peek()[1];
+            stack.push(new int[]{val, Math.min(val, currentMin)});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
